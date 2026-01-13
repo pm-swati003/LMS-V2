501e39d41eb91051f6b87f695e2d1685aba9bd71
SWATI <pmswati003@gmail.com>
2026-01-13 16:13:47 +0530

    In index.ts port is updated

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 
+
 const app = express();
 const httpServer = createServer(app);
 
@@ -85,14 +86,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+    httpServer.listen(port, () => {
+  log(`serving on port ${port}`);
+});
 })();
